db7f6c7281dec54df8a386642513d5ce20852858
mounirBen26 <mounir.bmh@hotmail.fr>
2024-06-03 21:56:44 +0100

    added localStorga logic

--- phases checker/script.js
+++ phases checker/script.js
@@ -10,11 +10,42 @@ generate.addEventListener("click", () => {
 function PapaParse() {
     let arrayobj = [];
     Papa.parse(file_input.files[0], {
+        download: true,
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: true,
         complete: function (results) {
             arrayobj = results.data;
+            let data = [];
+
+            for (let i = 0; i < arrayobj.length; i++) {
+                let item = arrayobj[i];
+                let dataItem = {
+                    reference: item.S_Lc_ref,
+                    phase1: item.S_Phase1,
+                    phase2: item.S_Phase2,
+                    phase3: item.S_Phase3
+                };
+                data.push(dataItem);
+            }
+
+            let lastMonthData = JSON.stringify(data);
             
-            console.log(parseFloat(arrayobj[1][4]));
+
+            // Check if the data already exists in local storage
+            let existingData = localStorage.getItem('lastMonthData');
+
+            if (!existingData || existingData !== lastMonthData) {
+                // Add the data to local storage
+                localStorage.setItem('lastMonthData', lastMonthData);
+                console.log('Data added to local storage');
+            } else {
+                // The data already exists in local storage
+                console.log('Data already exists in local storage');
+            }
         }
     });
-    // console.log(arrayobj);
+    let jsonString = localStorage.getItem('lastMonthData');
+    let getlastMonthData = JSON.parse(jsonString);
+    console.log('------------------',getlastMonthData)
 }
