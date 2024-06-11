39f67e288b3bd7c47f0396a57d453376d1f9f73f
mounirBen26 <mounir.bmh@hotmail.fr>
2024-06-11 01:13:00 +0100

    added current month export data logic

--- phases checker/script2.js
+++ phases checker/script2.js
@@ -7,12 +7,14 @@ const display = document.getElementById('display');
 
 function HandleExport() {
     fileInput.value = '';
-    display.style.backgroundColor = "yellow";
-    file_input_container.style.backgroundColor ="yellow"
+    container.style.backgroundColor = "#E7BCDE";
+    file_input_container.style.backgroundColor ="white";
+    role_fichier.textContent = "Charger le Fichier Export N-1";
+    title.textContent = "Energy Export Checker";
 }
 
 // adding the n-1 month export energydata
-function PapaParseExp() {
+function PapaParseExpLastMonth() {
     let arrayobj = [];
     Papa.parse(file_input.files[0], {
         download: true,
@@ -51,6 +53,84 @@ function PapaParseExp() {
             }
         }
     });
+}
 
+
+// adding the N month data
+function addCurrentMonthData() {
+    let getLastMonthDataExp = null
+    let getCurrentMonthDataExp = null
+    // reading the last month data from the localstorage
+    let lastMonthDataExp = localStorage.getItem('lastMonthDataExp');
+    getLastMonthDataExp = JSON.parse(lastMonthDataExp);
+
+    // parsing the new data from the new month
+    Papa.parse(file_input.files[0], {
+        download: true,
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: true,
+        complete: function (results) {
+            getCurrentMonthDataExp = results.data;
+            // comparing the new data with the last month data
+            if (getLastMonthDataExp) {
+                let index = 0
+                let nullChecker = null
+                const matchedData = getLastMonthDataExp.map(obj1 => {
+                    const matchingObj2 = getCurrentMonthDataExp.find(obj2 => obj2.S_Lc_ref === obj1.reference);
+                    if (matchingObj2) {
+                        // console.log(obj1.phase1 - matchingObj2.S_Phase1,obj1.phase2 - matchingObj2.S_Phase2,obj1.phase3 - matchingObj2.S_Phase3)
+                        expChecker = [matchingObj2.S_attni1e - obj1.phase1Exp, matchingObj2.S_attni2e - obj1.phase2Exp, matchingObj2.S_attni3e - obj1.phase3Exp]
+                        expChecker = expChecker.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+                        const tr = document.createElement('tr');
+                        if (expChecker !== 0) {
+                            console.log('----->>>>>', nullChecker)
+                            index++
+                            const tdIndex = document.createElement('td');
+                            tdIndex.textContent = index;
+                            tr.appendChild(tdIndex);
+                            const td = document.createElement('td');
+                            td.textContent = obj1.reference;
+                            tr.appendChild(td);
+                            const td1 = document.createElement('td');
+                            td1.textContent = matchingObj2.S_attni1e - obj1.phase1Exp;
+                            if (matchingObj2.S_attni1e - obj1.phase1Exp !== 0) {
+                                td1.style.backgroundColor = 'orange';
+                                td1.style.color = 'white';
+                            }
+                            tr.appendChild(td1);
+                            const td2 = document.createElement('td');
+                            td2.textContent = matchingObj2.S_attni2e - obj1.phase2Exp;
+                            if (matchingObj2.S_attni2e - obj1.phase2Exp !== 0) {
+                                td2.style.backgroundColor = 'orange';
+                                td2.style.color = 'white';
+                            }
+                            tr.appendChild(td2);
+                            const td3 = document.createElement('td');
+                            td3.textContent = matchingObj2.S_attni3e - obj1.phase3Exp;
+                            if (matchingObj2.S_attni3e - obj1.phase3Exp !== 0) {
+                                td3.style.backgroundColor = 'orange';
+                                td3.style.color = 'white';
+                            }
+                            tr.appendChild(td3);
+                            table.appendChild(tr);
+
+                        }
+                        console.log("index ------>",index)
+
+                    } else {
+                        return
+                    }
+
+                });
+            }
+
+        },
+
+    })
 }
 exportButton.addEventListener('click', HandleExport)
+generate.addEventListener('click',()=>{
+    // PapaParseExpLastMonth()
+    addCurrentMonthData()
+})
